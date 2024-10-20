81d39528e51b4e1da67b54c097eac78a3feb0b66
Md. Salauddin <salauddinahmed555@gmail.com>
2024-10-20 20:27:26 +0600

    Added icons and changed the website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,29 @@ import {
 } from '@clerk/nextjs'
 import './globals.css'
 import React from 'react'
+import type { Metadata } from 'next'
+
+import { Inter, Space_Grotesk} from 'next/font/google'
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ],
+  variable: '--font-inter',
+})
+const Space_Grotesk = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ],
+  variable: '--font-spaceGrotesk',
+})
+
+export const metadata: Metadata = {
+  title: 'Dev-Overflow NextJs',
+  description: 'A Next.js  Community driven app for asking and answering questions',
+  icons: {
+    icon: '/assets/images/site-logo.svg'
+  }
+}
+
+
 export default function RootLayout({
   children,
 }: {
@@ -15,7 +38,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className={``}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
