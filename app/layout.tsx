a2a5fb8360d3345627e0c1811a23c3ab22a54f8b
Md. Salauddin <salauddinahmed555@gmail.com>
2024-10-25 01:22:40 +0600

    created and exported themeprovider

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,61 +3,62 @@ import {
   SignInButton,
   SignedIn,
   SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
-import React from 'react'
-import type { Metadata } from 'next'
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
 
-import { Inter, Space_Grotesk} from 'next/font/google'
+import { Inter, Space_Grotesk } from "next/font/google";
+import { ThemeProvider } from "@/context/ThemeProvider";
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900' ],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 const spaceGrotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-spaceGrotesk',
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
 
 export const metadata: Metadata = {
-  title: 'Dev-Overflow NextJs',
-  description: 'A Next.js  Community driven app for asking and answering questions',
+  title: "Dev-Overflow NextJs",
+  description:
+    "A Next.js  Community driven app for asking and answering questions",
   icons: {
-    icon: '/assets/images/site-logo.svg'
-  }
-}
-
+    icon: "/assets/images/site-logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink:
-        'primary-text-gradient hover:text-primary-500',
-        
-      }
-    }}>
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}` }>
-          
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          {children}
-          <h1>hello vai</h1>
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {children}
+            <h1>hello vai</h1>
+          </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
 }
