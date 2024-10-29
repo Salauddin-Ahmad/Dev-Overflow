29b441747f9e57dd329ae94694059db74b7376ae
Md. Salauddin <salauddinahmed555@gmail.com>
2024-10-29 23:49:59 +0600

    some progress to navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full p-6 sm:px-12 dark:shadow-none ">
 
-export default Navbar
+      <Link href={"/"} className="flex items-center gap-1">
+
+      <Image 
+      src="public/assets/images/site-logo.svg" 
+      width={23}
+      height={23}
+      alt="Devflow"
+      />
+
+      
+        </Link>
+
+    </nav>
+  );
+};
+
+export default Navbar;
