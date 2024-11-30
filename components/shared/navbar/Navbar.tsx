e2bd278df3c403b6b53248f7dba2d6b4112018e4
Md. Salauddin <salauddinahmed555@gmail.com>
2024-11-30 20:15:52 +0600

    addded signed in and out from clerk in navbar

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,57 +1,3 @@
-// import Image from "next/image";
-// import Link from "next/link";
-// import React from "react";
-// import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-
-// const Navbar = () => {
-//   return (
-//     <nav className="flex-between background-light900_dark200 shadow-light-300 fixed z-50 w-full p-6 sm:px-12 dark:shadow-none">
-//       {/* Logo and Home Link */}
-//       <Link href={"/"} className="flex items-center gap-1">
-//         <Image
-//           src="puf/assets/images/site-logo.svg"
-//           width={23}
-//           height={23}
-//           alt="Devflow"
-//         />
-//         <p>
-//           {" "}
-//           Dev <span className="text-primary-500">Overflow</span>{" "}
-//         </p>
-//       </Link>
-
-//       {/* Right Section: Authenticated and Unauthenticated States */}
-//       <div className="flex items-center gap-4">
-//         {/* When Signed In */}
-//         <SignedIn>
-//           <UserButton
-//             afterSignOutUrl="/"
-//             appearance={{
-//               elements: {
-//                 avatarBox: "h-10 w-10",
-//               },
-//             }}
-//           />
-//         </SignedIn>
-
-//         {/* When Signed Out */}
-//         <SignedOut>
-//           <Link
-//             href="/sign-in"
-//             className="px-4 py-2 text-sm font-medium text-white bg-primary-500 rounded-lg"
-//           >
-//             Sign In
-//           </Link>
-//         </SignedOut>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
-
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -59,7 +5,7 @@ import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center background-light900_dark200 shadow-light-300 fixed z-50 w-full p-6 sm:px-12 dark:shadow-none">
+    <nav className="flex justify-between items-center bg-[#4a4a4a] fixed z-50 w-full p-6 sm:px-12 ">
       {/* Logo and Home Link */}
       <Link href="/" className="flex items-center gap-1">
         <Image
@@ -68,24 +14,35 @@ const Navbar = () => {
           height={23}
           alt="Devflow"
         />
-        <p className="text-lg font-bold">
-          Dev <span className="text-primary-500">Overflow</span>
+        <p className="h2-bold font-spaceGrotesk text-dark100 dark:text-light-900 ">
+          Dev <span className="text-orange-400">Overflow</span>
         </p>
       </Link>
 
+      Golobal search
+
       {/* Right Section: Authenticated and Unauthenticated States */}
       <div className="flex items-center gap-4">
+        Theme
+
+
+        MobileNav
         {/* When Signed In */}
         <SignedIn>
           <UserButton
-            afterSignOutUrl="/"
+          afterSwitchSessionUrl="/"
             appearance={{
               elements: {
                 avatarBox: "h-10 w-10",
               },
+              variables: {
+                colorPrimary: "#ff7000",
+              }
             }}
           />
         </SignedIn>
+
+         
 
         {/* When Signed Out */}
         <SignedOut>
@@ -96,6 +53,11 @@ const Navbar = () => {
             Sign In
           </Link>
         </SignedOut>
+      </div>
+
+
+      <div>
+
       </div>
     </nav>
   );
